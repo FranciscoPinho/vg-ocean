6f0fa0becde626e14bf15934616efc91afd5482c
Francisco Pinho <up201303744@fe.up.pt>
2018-08-24 19:11:40 +0100

    initial work on server

---
+++ backend/api/routes/Routes.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = function(app) {
+  var controller = require('../controllers/Controller');
+      // todoList Routes
+  app.route('/hltb/all')
+    .get(controller.getHLTBAll)
+};
