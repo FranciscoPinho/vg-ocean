042e51346bf2c2b8206b2474678d26b7c2902114
Unknown <mariadsda@gmail.com>
2018-09-19 23:18:42 +0100

    header finally finished (hopefully...)

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import {
     Icon,
     Image,
-    Dropdown
+    Dropdown,
+    Accordion
 } from "semantic-ui-react";
 import LoginModal from '../Login/LoginModal';
 import RegisterModal from '../Register/RegisterModal';
@@ -15,39 +16,59 @@ import './Header.css'
 
 
 class Heading extends Component {
+    state = { activeIndex: -1 }
+
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
     render() {
+        const { activeIndex } = this.state
         return (
             <div className="nav">
                 <div className="nav-header">
                     <div className="nav-title">
-                        <Link to="/"> 
-                            <Image src={logo} centered/>
-                        </Link> 
+                        <Link to="/">
+                            <Image src={logo} centered />
+                        </Link>
                     </div>
                 </div>
                 <div className="nav-btn">
                     <label htmlFor="nav-check">
-                        <Icon className="hamburgericon" inverted name='bars' size='large'/>
+                        <Icon className="hamburgericon" inverted name='bars' size='large' />
                     </label>
                 </div>
-                <input type="checkbox" id="nav-check"/>
+                <input type="checkbox" id="nav-check" />
                 <div className="nav-links">
                     <Link to="/feedback">My Collection</Link>
                     <Link to="/feedback">Explore</Link>
                     <Link to="/add+"> Add+</Link>
-                    <Dropdown item text="Create">
+                    <Dropdown className="create-dropdown" item text="Create">
                         <Dropdown.Menu>
-                            <Dropdown.Item>List</Dropdown.Item>
-                            <Dropdown.Item>Article</Dropdown.Item>
+                            <Dropdown.Item id="header-dropdown-item"><Link to="/feedback">List</Link></Dropdown.Item>
+                            <Dropdown.Item id="header-dropdown-item"><Link to="/feedback">Article</Link></Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
+                    <Accordion className="create-accordion" fluid>
+                        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+                            <Icon name='dropdown' />
+                            Create
+                    </Accordion.Title>
+                        <Accordion.Content active={activeIndex === 0}>
+                        <Link to="/feedback">List</Link>
+                        <Link to="/feedback">Article</Link>
+                        </Accordion.Content>
+                    </Accordion>
                     <Link to="/add+1"> Premium</Link>
                     <Link to="/feedback">Feedback</Link>
                 </div>
                 <div className="nav-log-modal">
-                    <LoginModal/>
+                    <LoginModal />
                 </div>
-                <RegisterModal/>
+                <RegisterModal />
             </div>
         )
     }
